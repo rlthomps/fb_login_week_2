4812b8e254e3241f110e1a59a0209013b5eadc1f
rlthomps <rlthomps@rlthomps-macbookair2.roam.corp.google.com>
2014-06-22 13:22:38 -0700

    Initial Commit

---
+++ fb_login_week_2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fb_login_week_2
+//
+//  Created by Robert Thompson on 6/22/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
